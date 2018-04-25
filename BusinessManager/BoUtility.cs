e938f45096506236f0f3b1ffc8982dd97e401c14
Anitha <anitha.kodati@dinoosys.com>
2018-04-25 17:28:18 +0530

    added get menus by role related methods

--- BusinessManager/BoUtility.cs
+++ BusinessManager/BoUtility.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DataManager;
+using System.Data;
 
 namespace BusinessManager
 {
-    class BoUtility
+   
+   public class BoUtility
     {
+       DoUtility _objDOUtility = new DoUtility();
+
+
+        #region DynamicMenuData
+        // added by Anitha on 25/04/2018
+
+
+       public DataSet GetMenusByRole(int RoleId, int CompanyId)
+        {
+            return _objDOUtility.GetMenusByRole(RoleId, CompanyId);
+        }
+
+        #endregion
+
     }
 }
--- DataManager/DoUtility.cs
+++ DataManager/DoUtility.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 
 namespace DataManager
 {
-    class DoUtility
+    public class DoUtility : DataUtility
     {
+        #region DynamicMenuData
+        // added by Anitha on 25/04/2018
+        public DataSet GetMenusByRole(int RoleId, int CompanyId)
+        {
+            Hashtable htParams = new Hashtable
+                                     {
+                                          
+                                         {"@inRoleId",RoleId},
+                                         {"@inCompanyId",CompanyId},
+                                         // {"@BranchId",BranchId}
+                                     };
+            return ExecuteDataSet("menuaccess_by_role", htParams);
+        }
+        #endregion
     }
 }
