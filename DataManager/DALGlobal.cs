e6708478861c537452e9f3e9946c890060bb36c2
mounikaDIno <36558241+mounikaDIno@users.noreply.github.com>
2018-05-07 14:14:30 +0530

    exception class

--- DataManager/DALGlobal.cs
+++ DataManager/DALGlobal.cs
@@ -16,34 +16,37 @@ namespace DataManager
       //public static class ExceptionLogging
       //{
 
-          private static String exepurl;
-          static SqlConnection con;
+        #region ExceptionLogging
 
-          private static void connecttion()
-          {
-              string constr = ConfigurationManager.ConnectionStrings["ConnString"].ToString();
-              con = new SqlConnection(constr);
-              con.Open();
-          }
-          public static void SendExcepToDB(Exception exdb)
-          {
-              //if (HttpContext.Current.Session["UserLoginId"] != null)
-              //{
-                  //int userid = Convert.ToInt32(HttpContext.Current.Session["UserLoginId"].ToString());
-                  connecttion();
-                  exepurl = context.Current.Request.Url.ToString();
-                  SqlCommand com = new SqlCommand("Exception_ExceptionLoggingToDataBase", con);
-                  com.CommandType = CommandType.StoredProcedure;
-                  com.Parameters.AddWithValue("@ExceptionMsg", exdb.Message.ToString());
-                  com.Parameters.AddWithValue("@ExceptionType", exdb.GetType().Name.ToString());
-                  com.Parameters.AddWithValue("@ExceptionURL", exepurl);
-                  com.Parameters.AddWithValue("@ExceptionSource", exdb.StackTrace.ToString());
-                  com.Parameters.AddWithValue("@UserId", 1);
-                  com.ExecuteNonQuery();
+        private static String exepurl;
+        static SqlConnection con;
+
+        private static void connecttion()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["ConnString"].ToString();
+            con = new SqlConnection(constr);
+            con.Open();
+        }
+        public static void SendExcepToDB(Exception exdb)
+        {
+            //if (HttpContext.Current.Session["UserLoginId"] != null)
+            //{
+            //int userid = Convert.ToInt32(HttpContext.Current.Session["UserLoginId"].ToString());
+            connecttion();
+            exepurl = context.Current.Request.Url.ToString();
+            SqlCommand com = new SqlCommand("Exception_ExceptionLoggingToDataBase", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@ExceptionMsg", exdb.Message.ToString());
+            com.Parameters.AddWithValue("@ExceptionType", exdb.GetType().Name.ToString());
+            com.Parameters.AddWithValue("@ExceptionURL", exepurl);
+            com.Parameters.AddWithValue("@ExceptionSource", exdb.StackTrace.ToString());
+            com.Parameters.AddWithValue("@UserId", 1);
+            com.ExecuteNonQuery();
             //  }
 
 
-          }
+        } 
+        #endregion
 
 
       //}  
